a9db84e846888b76de91a6d48511b9171c5458ba
Avanish Vaghela <avanish@metadataconsulting.co.uk>
2020-09-27 16:02:39 +0100

    EX-1930/EX-1940 Updated test runner class to run LoginPage feature tests

--- src/test/java/works/metadata/qa/tests/TestRunner.java
+++ src/test/java/works/metadata/qa/tests/TestRunner.java
@@ -1,17 +1,17 @@
 package works.metadata.qa.tests;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
+        import io.cucumber.junit.Cucumber;
+        import io.cucumber.junit.CucumberOptions;
+        import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "",
-        glue = "",
+        features = "src/test/resources/features/metadataExchange/LoginPage/LoginPage.feature",
+        glue = "works.metadata.qa.tests.steps",
         plugin = {"pretty", "summary", "de.monochromata.cucumber.report.PrettyReports:target/cucumber-reports/html", "html:target/cucumber-reports/html/cucumber.html", "json:target/cucumber-reports/json/cucumber.json"},
         monochrome = false,
         dryRun = false,
-        tags = ""
+        tags = "@SmokeTest"
 )
 public class TestRunner {
 }
