69e1bb0587e9f69d3f21e16771768c578f818556
Avanish Vaghela <avanish@metadataconsulting.co.uk>
2020-10-06 13:41:18 +0100

    EX-1930/EX-1938 Reduced Page load time out and Implicit wait timeout

--- src/test/java/works/metadata/qa/tests/TestConfigurations.java
+++ src/test/java/works/metadata/qa/tests/TestConfigurations.java
@@ -1,6 +1,6 @@
 package works.metadata.qa.tests;
 
 public class TestConfigurations {
-    public static final int PAGE_LOAD_TIMEOUT = 10;
-    public static final int IMPLICIT_WAIT = 10;
+    public static final int PAGE_LOAD_TIMEOUT = 5;
+    public static final int IMPLICIT_WAIT = 3;
 }
